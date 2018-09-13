2309b7102ec9662f3d0e58437ac1e04d44742525
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-09-13 15:15:46 +0530

    booking controller update

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -11,31 +11,57 @@ class BookController < ApplicationController
   def flight
   	@detail = nil
   	@codes = []
-  	IataCode.all.each do |x|
-    @codes << [ x.city , x.airport , x.code]
-	end
+  	
+    IataCode.all.each do |x|
+      @codes << [ x.city , x.airport , x.code]
+	  end
     
+   
+
+    p1 = params[:start_date]
+    
+    if p1!=nil
+    date1 = p1["year"]
+      if p1["month"].to_i < 10
+         date1 = date1 + "0" +p1["month"]
+      else
+        date1 = date1 + p1["month"]
+      end
+      
+      if p1["day"].to_i < 10
+         date1 = date1 + "0" +p1["day"]
+      else
+        date1 = date1 + p1["day"]
+      end
+    end
+
+   
+   
+
     source = params[:src].to_s
     destination = params[:dest].to_s
-    dateofdeparture = params[:dept].to_s
-    dateofarrival = params[:arr].to_s
+    dateofdeparture = date1.to_s
+    dateofarrival = ""
     seatingclass = params[:class].to_s
     adults = params[:adults].to_s
     children = params[:child].to_s
     infants = params[:infants].to_s
     counter = params[:counter].to_s 		# 0 - international , 100 - domestic
+    
+   
 
     @total = adults.to_i + children.to_i + infants.to_i
     @param1 = adults.to_i
     @param2 = children.to_i
     @param3 = infants.to_i
+   
+
     begin
-        
     	uri = URI('https://developer.goibibo.com/api/search/')
-		params = {  "app_id" => "01d613cc" ,
-					"app_key" => "2d974c23eaa3034771edbe80db153a34" ,
-				   "source" => source ,
-					"destination" => destination, 
+	   	params = {  "app_id" => "01d613cc" ,
+		    			    "app_key" => "2d974c23eaa3034771edbe80db153a34" ,
+				          "source" => source ,
+					        "destination" => destination, 
 	                "dateofdeparture" => dateofdeparture, 
 	                "dateofarrival" => dateofarrival ,
 	                "seatingclass" => seatingclass,
@@ -48,11 +74,10 @@ class BookController < ApplicationController
 		res = Net::HTTP.get_response(uri)
 		# => puts res.body if res.is_a?(Net::HTTPSuccess)
 		@result = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-        
+       
 	    rescue => e
 	        puts "failed #{e}"
-
-	end
+     end
 	
  #  respond_to do |format|
  #      format.html 
